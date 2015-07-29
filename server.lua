14802d75e4f90df5aa2c0703905f2d50232be7fa
Robert Foss <dev@robertfoss.se>
2015-07-29 20:21:52 +0200

    Added basic wifi credentials saving.

--- server.lua
+++ server.lua
@@ -6,29 +6,39 @@ XChip's NodeMCU IDE
 ]]--
 
 srv=net.createServer(net.TCP) 
-srv:listen(80,function(conn) 
+srv:listen(80, function(conn) 
 
    local rnrn=0
    local Status = 0
-   local DataToGet = 0
+   local responseBytes = 0
    local method=""
    local url=""
    local vars=""
 
-   conn:on("receive",function(conn,payload)
+   conn:on("receive",function(conn, payload)
   
     if Status==0 then
         _, _, method, url, vars = string.find(payload, "([A-Z]+) /([^?]*)%??(.*) HTTP")
         -- print(method, url, vars)                          
     end
+    
+    print("Heap   : " .. node.heap())
+    print("Payload: " .. payload)
+    print("Method : " .. method)
+    print("URL    : " .. url)
+    print("Vars   : " .. vars  .. "\n\n\n")
 
-	if url==nil then
-		url="index.htm"
+
+    -- Check if wifi-credentials have been supplied
+    if vars~=nil and parse_wifi_credentials(vars) then
+        node.restart()
+    end
+
+	if url==nil or url == "" then
+		url="index.html"
+        responseBytes = 0
 	end
-	
-	if url=="" then
-		url="index.htm"
-	end
+
 	
 	-- some ugly magic for Apple IOS Devices
 	if string.find(url, "/") ~= nil then
@@ -38,59 +48,28 @@ srv:listen(80,function(conn)
 	 url=string.sub(url, string.len(url)-(a-2))
 	 --print ("Neue URL= " .. url)
 	end
-		
-	if string.len(url)>= 25 then
-		url = string.sub (url,1,25)
-	--	print ("cut down URL")
-	end
-	
-   
-    DataToGet = -1
+
 
     if url == "favicon.ico" then
         conn:send("HTTP/1.1 404 file not found")
+        responseBytes = -1
         return
     end    
 
 	
     conn:send("HTTP/1.1 200 OK\r\n\r\n")
 	
-	if url==nil or url=="" then
-		url="index.html"
-	end
-	
-	
-	local foundmatch = 0
-	local a = {'index.html'}
-	for _,v in pairs(a) do
-		if v == url then
-			foundmatch=1
-			-- print ("Found " .. v)
-			break
-		end
-	end
-
-    if foundmatch == 0 then
-    	-- print ("Found no match, setting index")
-        url="index.html"
-    end	
-
-    -- it wants a file in particular
-    if url~="" then
-        DataToGet = 0
-        return
-    end    
   end)
   
   conn:on("sent",function(conn) 
-    if DataToGet>=0 and method=="GET" then
+    if responseBytes>=0 and method=="GET" then
         if file.open(url, "r") then            
-            file.seek("set", DataToGet)
+            file.seek("set", responseBytes)
             local line=file.read(512)
             file.close()
             if line then
                 conn:send(line)
-                DataToGet = DataToGet + 512    
+                responseBytes = responseBytes + 512    
 
                 if (string.len(line)==512) then
                     return
@@ -102,7 +81,27 @@ srv:listen(80,function(conn)
     conn:close() 
   end)
 end)
+print("HTTP Server: listening. Heap: ", node.heap())
 
-	
-print("listening, free:", node.heap())
+
+function parse_wifi_credentials(vars)
+    if vars == nil or vars == "" then
+        return false
+    end
+    local _, _, wifi_ssid = string.find(vars, "wifi_ssid\=([^&]+)")
+    local _, _, wifi_password = string.find(vars, "wifi_password\=([^&]+)")
+    if wifi_ssid == nil or wifi_ssid == "" or wifi_password == nil then
+        return false
+    end
+
+    print("wifi_ssid     : " .. wifi_ssid)
+    print("wifi_passwordd: " .. wifi_password)
+
+    file.open("wifi_credentials", "w+")
+    file.writeline(wifi_ssid)
+    file.writeline(wifi_password)
+    file.flush()
+    file.close()
+    return true
+end
 
